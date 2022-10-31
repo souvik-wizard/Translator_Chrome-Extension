4dff75be23ac1209302d0a42dd517a6c50fa2a4e
souvik-wizard <dassouvik3327@gmail.com>
2022-10-31 18:38:57 +0530

    edit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Fetching
+
 const options = {
 	method: 'POST',
 	headers: {
@@ -12,3 +14,30 @@ fetch('https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=bn&a
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+// language
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'b772aa655cmshd6b3aab9ad7d7e0p10a524jsn57a88a590afb',
+// 		'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com',
+// 		'Accept-Encoding': 'null'
+// 	}
+// };
+
+// fetch('https://microsoft-translator-text.p.rapidapi.com/languages?api-version=3.0', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+
+
+	// Body 
+	let drop=document.getElementById("dropdown")
+	drop.addEventListener('click',function (){
+		// console.log("clicked")
+		for (let i=0;i<10;i++){
+			drop.createElement('option')	  
+		}
+	})
